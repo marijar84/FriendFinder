d9e703802ef5e2aa2c2b11e276b003eb80c3efbb
Maribel Jaramillo <marijar84@gmail.com>
2018-05-24 18:06:47 -0700

    Homework

--- app/data/friend.js
+++ app/data/friend.js
@@ -3,13 +3,34 @@
 var tableArray = [
     {
         "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "photo": "https://www.vbout.com/images/persona/buyer-persona-image1.png",
         "scores": [
-            5,
-            1,
-            4,
-            4,
-            1
+            "5",
+            "1",
+            "4",
+            "4",
+            "1",
+            "5",
+            "1",
+            "4",
+            "4",
+            "1"
+        ]
+    },
+    {
+        "name": "Mary",
+        "photo": "http://www.weblab24.it/wp-content/uploads/2017/05/buyer-persona-image3.png",
+        "scores": [
+            "4",
+            "5",
+            "2",
+            "3",
+            "2",
+            "1",
+            "2",
+            "3",
+            "3",
+            "2"
         ]
     }
   ];
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,7 +7,32 @@ module.exports = function (app) {
 
     app.post('/api/friends', function (req, res) {
         console.log("req", req.body);
+
+        var newFriend = req.body;
+
+        var differences = [];
+
+        var bestFriend = 0;
+
+        for(var friend = 0; friend < friendList.length; friend++){
+
+            var scoreDifference = 0;
+            for(var scores = 0; scores < newFriend.scores.length; scores++){
+                scoreDifference += (Math.abs(parseInt(friendList[friend].scores[scores]) - parseInt(newFriend[scores])));
+            }
+
+            differences.push(scoreDifference);
+        }
+
+        for(var item = 0; item < differences.length; item++)
+        {
+            if(differences[item] <= differences[bestFriend])
+            {
+                bestFriend = item;
+            }            
+        }
+
         friendList.push(req.body);
-        res.json(friendList);
+        res.json(friendList[bestFriend]);
     });
 };
